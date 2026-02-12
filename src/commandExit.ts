dfd07a31da3565ff0af6583fb39d6d3d29bc3c47
Nicholas Doublet <thedoubiedev@gmail.com>
2026-02-12 13:57:21 +1100

    Revert "add initial repl"
    This reverts commit f6f01994842186fb031307b5cac075a05cc7fb8e.

--- src/commandExit.ts
+++
@@ -1,7 +0,0 @@
-import type { State } from "./state";
-
-export async function commandExit(state: State) {
-    console.log("Closing...");
-    state.readline.close();
-    process.exit(0);
-};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-import { initState } from "./state";
-import { startREPL } from "./repl";
+import { type Config, createConfig, readConfig, updateConfig } from './config';
 
-async function main() {
-    const state = initState();
-    await startREPL(state);
+function main() {
+    const firstConfig: Config = {
+        creator: "johndoe",
+        numPostsToScrape: 90,
+        scrapeByYear: true,
+        scrapeComments: true,
+        scrapeReplies: true,
+    }
+
+    const updatedConfig: Partial<Config> = {
+        creator: 'updatedcreator',
+    }
+
+    console.log("writing first config...");
+    createConfig(firstConfig);
+    console.log("reading first config...");
+    console.log("config:", readConfig());
+    console.log("updating config...");
+    updateConfig(updatedConfig);
+    console.log("reading updated config...");
+    console.log("config:", readConfig());
 }
 
 main();
